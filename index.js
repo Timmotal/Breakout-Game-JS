7e07d29a527541696769b38a6b1dad409db63c71
move style to head <timmortal44@yahoo.com>
2022-09-08 23:32:20 +0100

    add first row of bricks

--- index.js
+++ index.js
@@ -28,4 +28,12 @@ class Block {
     }
 }
 
-const blocks = [] 
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+
+
+] 
