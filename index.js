c31965f9fffe47db98e0cd112da72419ef8d60d6
move style to head <timmortal44@yahoo.com>
2022-09-16 23:54:43 +0100

    add ballCurrentPosition for y axis

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ grid.appendChild(ball)
 function moveBall() {
     
     ballCurrentPosition[0] += xDirection // adding 2px per set time in "x-axis" left to right, Horizontal
-   
+    ballCurrentPosition[1] += yDirection // adding 2px per set time in "y-axis"  bottom to up, Vertical
     
   
   
